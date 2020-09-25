cc3134c3d3a18aa292da0fc4b495282ff49300bd
mengkki <guswnl0610@naver.com>
2020-09-25 15:56:39 +0900

    starting step6

--- polls/models.py
+++ polls/models.py
@@ -7,15 +7,17 @@ import datetime
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    def __Str__(self):
+    def __str__(self):
         return self.question_text
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE) #cascade 얘랑 연결된 question이 사라지면 얘도 사라진다
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    def __Str__(self):
+    def __str__(self):
         return self.choice_text
+
--- polls/views.py
+++ polls/views.py
@@ -3,20 +3,25 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
-#from django.http import Http404
+from django.http import Http404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 
 # Create your views here.
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.filter(pub_date__lte = timezone.now()).order_by('-pub_date')[:5]
+
     
     template = loader.get_template('polls/index.html')
     context = {'latest_question_list' : latest_question_list,}
     #return HttpResponse(template.render(context,request))
+
     return render(request, 'polls/index.html', context)
+
 
 # class IndexView(generic.ListView):
 #     template_name = 'polls/index.html'
@@ -32,12 +37,21 @@ def detail(request, question_id):
     #     raise Http404("Question does not exist")
     # return render(request,'polls/detail.html',{'question':question})
     # return HttpResponse("You're looking at question %s." % question_id)
-    question = get_object_or_404(Question, pk=question_id)
+    
+    # question = get_object_or_404(Question, pk=question_id)
+    # question = Question.objects.filter(pub_date__lte=timezone.now())
+
+    try:
+        question = Question.objects.filter(pub_date__lte=timezone.now()).get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question dos not exist")
     return render(request, 'polls/detail.html', {'question':question})
 
 # class DetailView(generic.DetailView):
 #     model = Question
 #     template_name = 'polls/detail.html'
+#     def get_queryset(self):
+#         return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 def results(request, question_id):
